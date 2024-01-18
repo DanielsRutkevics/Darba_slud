126d96a637fc92fd68fb39f6d0809b0ad7625f9b
231RDB290 <144444832+DanielsRutkevics@users.noreply.github.com>
2024-01-18 23:19:52 +0700

    final

--- proj.py
+++ proj.py
@@ -16,81 +16,102 @@ url = "https://cv.lv/lv"
 
 driver.get(url)
 
+# nodrošina, ka mājaslapa pabeigs lādties pirms turpmākām darbībām
 WebDriverWait(driver, 5).until(ES.presence_of_element_located((By.CLASS_NAME, "jsx-4189752321.close-modal-button")))
 
-
+# reklāmas aizvēršana
 poga = driver.find_element(By.CLASS_NAME, "jsx-4189752321.close-modal-button")
 poga.click()
 
-
+# atvērt izvēli
 poga1 = driver.find_element(By.CLASS_NAME, "css-19bqh2r")
 poga1.click()
 time.sleep(1)
+
+# izvēlēties Rīgu
 poga = driver.find_element(By.ID, "react-select-2-option-1-0")
 poga.click()
 time.sleep(1)
+
+# atvērt izvēli
 poga1 = driver.find_element(By.CLASS_NAME, "css-19bqh2r")
 poga1.click()
+
+# izvēlēties attālināti
 poga2 = driver.find_element(By.ID, "react-select-2-option-0-0")
 poga2.click()
 
 
-
+# atvērt izvēli otro
 uzleju = driver.find_elements(By.CLASS_NAME, "css-19bqh2r")
 uzleju[1].click()
 time.sleep(1)
 
-IT = driver.find_element(By.ID, "react-select-3-option-0-0")
+# IT
+IT = driver.find_element(By.ID, "react-select-3-option-1-0")
 IT.click()
 time.sleep(1)
 
+# atķeksēt izvēles logu
 kast=driver.find_element(By.CLASS_NAME, "jsx-1690969694.input-checkbox")
 kast.click()
 time.sleep(1)
 
+# izvērst meklēšanas opcijas
 vairak = driver.find_element(By.CLASS_NAME, "jsx-2273220519.btn-plain.btn-plain--standard")
 vairak.click()
 time.sleep(1)
 
+# atvērt izvēli trešo
 zleju = driver.find_elements(By.CLASS_NAME, "css-19bqh2r")
 zleju[4].click()
 time.sleep(3)
+
 pus= driver.find_element(By.ID, "react-select-5-option-2")
 pus.click()
 time.sleep(3)
+
 zleju = driver.find_elements(By.CLASS_NAME, "css-19bqh2r")
 zleju[4].click()
+
 pecmac = driver.find_element(By.ID, "react-select-5-option-4")
 pecmac.click()
+
 zleju = driver.find_elements(By.CLASS_NAME, "css-19bqh2r")
 zleju[4].click()
+
 prakse = driver.find_element(By.ID, "react-select-5-option-5")
 prakse.click()
 
 
-
+# palielināt pārlūkprogrammas logu
 driver.maximize_window()
 time.sleep(2)
 
+# uzspiest rādīt rezultātus
 m=driver.find_element(By.CLASS_NAME, "jsx-2818744897.search-form-footer__item")
 m.click()
 
+# izslēgt cookies
 cookie=driver.find_element(By.CLASS_NAME,"cookie-consent-button")
 cookie.click()
 
-
 time.sleep(5)
-# WebDriverWait(driver, 5).until(ES.presence_of_element_located((By.CLASS_NAME, "jsx-4189752321.close-modal-button")))
-# # url2 = "https://cv.lv/lv/search?limit=20&offset=0&categories%5B0%5D=INFORMATION_TECHNOLOGY&workTimes%5B0%5D=PART_TIME&workTimes%5B1%5D=WORK_AFTER_CLASSES&workTimes%5B2%5D=PRACTICE&towns%5B0%5D=543&fuzzy=false&suitableForRefugees=false&isHourlySalary=false&isRemoteWork=true&isQuickApply=false"
-# # saturs = requests.get(url2)
+
+# atjaunināt pašreizējās mājaslapas saturu
 driver.refresh()
+
+# izveido html objektu
 zup= bs4.BeautifulSoup(driver.page_source,"html.parser")
 
+# atrod visus span elementus ar noteiktu klasi
 darba_nosaukumi = zup.find_all('span',class_="jsx-3024910437 vacancy-item__title")
 
+# atver teksta failu un nolasa
 with open("profesijas_sar.txt", "r") as file:
     filesat = file.read()
 
+# ja failā nav atrastās profesijas tā tiek pievienota teksta failam
 with open("profesijas_sar.txt", "a") as file:
     for darbs in darba_nosaukumi:
         if str(darbs) not in filesat:
@@ -98,5 +119,5 @@ with open("profesijas_sar.txt", "a") as file:
 
 
 
-            
+# nodrošina lai pārlūkprogramma neaizveras un var turpināt skatīties sludinājumus           
 input()
