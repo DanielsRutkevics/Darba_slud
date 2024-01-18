85f7fae3d5d6dfdbc0807b69aa706a72f52bf699
231RDB290 <144444832+DanielsRutkevics@users.noreply.github.com>
2024-01-18 22:48:22 +0700

    final

--- proj.py
+++ proj.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ES
 import time
+import bs4
 
 service = Service()
 option = webdriver.ChromeOptions()
@@ -39,7 +40,7 @@ uzleju = driver.find_elements(By.CLASS_NAME, "css-19bqh2r")
 uzleju[1].click()
 time.sleep(1)
 
-IT = driver.find_element(By.ID, "react-select-3-option-1-0")
+IT = driver.find_element(By.ID, "react-select-3-option-0-0")
 IT.click()
 time.sleep(1)
 
@@ -78,8 +79,24 @@ cookie=driver.find_element(By.CLASS_NAME,"cookie-consent-button")
 cookie.click()
 
 
+time.sleep(5)
+# WebDriverWait(driver, 5).until(ES.presence_of_element_located((By.CLASS_NAME, "jsx-4189752321.close-modal-button")))
+# # url2 = "https://cv.lv/lv/search?limit=20&offset=0&categories%5B0%5D=INFORMATION_TECHNOLOGY&workTimes%5B0%5D=PART_TIME&workTimes%5B1%5D=WORK_AFTER_CLASSES&workTimes%5B2%5D=PRACTICE&towns%5B0%5D=543&fuzzy=false&suitableForRefugees=false&isHourlySalary=false&isRemoteWork=true&isQuickApply=false"
+# # saturs = requests.get(url2)
+driver.refresh()
+zup= bs4.BeautifulSoup(driver.page_source,"html.parser")
+
+darba_nosaukumi = zup.find_all('span',class_="jsx-3024910437 vacancy-item__title")
+
+with open("profesijas_sar.txt", "r") as file:
+    filesat = file.read()
+
+with open("profesijas_sar.txt", "a") as file:
+    for darbs in darba_nosaukumi:
+        if str(darbs) not in filesat:
+            file.write(str(darbs.text) + "\n")
 
 
+
+            
 input()
-time.sleep(4)
-
